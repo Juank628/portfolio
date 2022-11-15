054a7dc1bb83624a70346a81bc775c73fcad2b4a
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-11-15 18:11:31 -0300

    js to get project info done

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ const mobileMenu = document.getElementById('mobile_menu');
 const closeMenuIcon = document.getElementById('close_menu_icon');
 const mobileLinkList = document.getElementById('mobile_link_list');
 
+const projects = [
+  {
+    id: 'first-project',
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'asdjoas',
+    img: '',
+    technologies: [],
+    linkLive: '',
+    linkSource: ''
+  },
+  {
+    id: 'second-project',
+    name: 'Multi-Post Stories Gain+Glory 2',
+    description: 'asdjoas',
+    img: '',
+    technologies: [],
+    linkLive: '',
+    linkSource: ''
+  }
+]
+
 mobileMenuIcon.addEventListener('click', () => {
   const { classList } = mobileMenu;
   classList.remove('no-visible');
@@ -17,3 +38,13 @@ mobileLinkList.addEventListener('click', () => {
   const { classList } = mobileMenu;
   classList.add('no-visible');
 });
+
+document.querySelectorAll('.project-btn').forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const projectElement = e.currentTarget.parentNode
+    const projectInfo = projects.filter((project) => project.id === projectElement.id)
+    
+  })
+})
+
+
